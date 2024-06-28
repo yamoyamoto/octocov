c166a7bf7ed364ce1ac57a3bd70a3cf62fabba8b
tomoya-kawaguchi <tomoya.kawaguchi@dena.jp>
2024-06-28 11:44:51 +0900

    Merge pull request #1 from yamoyamoto/feat/add-comment-title-config
    add: comment title config

--- report/option.go
+++ report/option.go
@@ -4,6 +4,7 @@ import "golang.org/x/text/language"
 
 type Options struct {
 	Locale *language.Tag
+	Title  string
 }
 
 type Option func(*Options)
@@ -11,5 +12,11 @@ type Option func(*Options)
 func Locale(locale *language.Tag) Option {
 	return func(args *Options) {
 		args.Locale = locale
+	}
+}
+
+func Title(title string) Option {
+	return func(args *Options) {
+		args.Title = title
 	}
 }
